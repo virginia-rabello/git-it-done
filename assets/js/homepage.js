f858a7a2ad655c5325eb36e437aebdbb0bf6a49c
Virginia <virginia.rabello2107@gmail.com>
2021-04-20 15:19:08 -0500

    issues covered

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -45,16 +45,22 @@ if (repos.length === 0) {
 repoContainerEl.textContent = "";
 repoSearchTerm.textContent = searchTerm;
 for (var i = 0; i < repos.length; i++) {
-  // format repo name
-  var repoName = repos[i].owner.login + "/" + repos[i].name;
+   // format repo name
+   var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-  // create a container for each repo
-  var repoEl = document.createElement("div");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
+   // create a container for each repo
+   var repoEl = document.createElement("a");
+   repoEl.classList = "list-item flex-row justify-space-between align-center";
+   repoEl.setAttribute("href", "./single-repo.html");
 
-  // create a span element to hold repository name
+   // create a span element to hold repository name
   var titleEl = document.createElement("span");
   titleEl.textContent = repoName;
+
+  // create a link for each repo
+  var repoEl = document.createElement("a");
+  repoEl.classList = "list-item flex-row justify-space-between align-center";
+  repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
   // append to container
   repoEl.appendChild(titleEl);
